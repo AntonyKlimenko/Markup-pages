6fca5cd3a6372101ecbe4a95e6a78ef7fd406729
antonyklimenko <antonyklimenko@gmail.com>
2023-08-28 21:52:50 +0300

    Adding makeup, page-proofs

---
+++ Xiaomi Himo/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.bike-slider, .slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        speed: 500,
+        cssEase: 'linear'
+    });
+});
